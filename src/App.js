520a345f98bf8f10e7b36729e85c72cc3052e53a
Poovannan M <42384112+Poovannan98@users.noreply.github.com>
2022-10-21 11:58:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import {useFormik} from "formik";
+import * as yup from 'yup';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <>
+  <Form/> 
+  </>
+}
+
+const formValidationSchema = yup.object({
+  email : yup.string().min(8, "Need a longer email😊")
+  .required(),
+  password : yup.string().min(8, "Passwowrd must be 8")
+  .max(12, "should not grater 12")
+  .required(),
+})
+
+function Form(){
+  const formik = useFormik({
+    initialValues: {
+      email: "poo",
+      password: "abc",
+    },
+    validationSchema: formValidationSchema,
+    onSubmit:(values) => {
+      console.log("Form Values", values);
+    },
+  });
+  return <>
+  <form onSubmit={formik.handleSubmit}>
+    <input
+      name='email' 
+      value={formik.values.email}
+      onChange={formik.handleChange} 
+      onBlur={formik.handleBlur}
+      type='email'     
+      placeholder='username'
+    />
+    {formik.touched.email && formik.errors.email ? formik.errors.email : null}
+    <br/>
+    <input
+      name='password' 
+      value={formik.values.password}
+      onChange={formik.handleChange}
+      onBlur={formik.handleBlur}
+      type='password' 
+      placeholder='password'
+    />
+    {formik.errors.password && formik.errors.password ? formik.errors.password  : null}
+    <br/>
+    <button type='submit'>Sumit</button>
+    {/* <div>
+      Error
+      <pre>{JSON.stringify(formik.errors)}</pre>
+      Touched
+      <pre>{JSON.stringify(formik.touched)}</pre>
+    </div> */}
+  </form>
+  </>
 }
 
 export default App;
